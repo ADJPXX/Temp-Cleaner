8414c1bc005039acbe7eb8a78086a5c24a0850e6
Adriel Jacobus Pereira <adrieljp2005@hotmail.com>
2026-09-09 03:04:19 -0300

    feat: refactor cleanup operations into services

---
+++ TempCleaner/CleanService.cs
@@ -0,0 +1,138 @@
+﻿using System.Diagnostics;
+
+namespace TempCleaner;
+
+public static class CleanService
+{
+    public static void ClearFiles(string temp)
+    {
+        foreach (var file in Directory.GetFiles(temp))
+        {
+            try
+            {
+                LogService.AddLog($"ARQUIVO ALVO: {file}");
+                
+                Console.WriteLine($"ARQUIVO ALVO: {file}");
+                
+                File.SetAttributes(file, FileAttributes.Normal);
+                
+                File.Delete(file);
+
+                if (!File.Exists(file))
+                {
+                    LogService.AddLog("ARQUIVO DELETADO!\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogService.AddLog($"FALHA AO DELETAR O ARQUIVO, ERRO: {ex.Message}\n");
+                
+                Console.WriteLine($"FALHA AO DELETAR O ARQUIVO: {file}");
+                
+                Console.WriteLine($"ERRO: {ex.Message}\n");
+            }
+        }
+    }
+
+
+    public static void ClearFolders(string temp)
+    {
+        foreach (var file in Directory.GetDirectories(temp))
+        {
+            try
+            {
+                LogService.AddLog($"PASTA ALVO: {file}");
+                
+                Console.WriteLine($"PASTA ALVO: {file}");
+                
+                Directory.Delete(file, true);
+
+                if (!Directory.Exists(file))
+                {
+                    LogService.AddLog("PASTA DELETADA!\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogService.AddLog($"FALHA AO DELETAR A PASTA, ERRO: {ex.Message}\n");
+                
+                Console.WriteLine($"FALHA AO DELETAR A PASTA: {file}");
+                
+                Console.WriteLine($"ERRO: {ex.Message}\n");
+            }
+        }
+    }
+
+
+    public static void ClearRecycleBin()
+    {
+        try
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.Name.Contains(@"G:\"))
+                {
+                    continue;
+                }
+
+                var lixeiraDrive = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c rd /s /q \"{drive.Name}$RECYCLE.BIN\"",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                });
+                
+                lixeiraDrive?.WaitForExit();
+
+                if (lixeiraDrive?.ExitCode == 0)
+                {
+                    LogService.AddLog($"LIXEIRA DELETADA DO DRIVE: {drive.Name}");
+                }
+                else
+                {
+                    LogService.AddLog($"FALHA AO DELETAR A LIXEIRA DO DRIVE: {drive.Name} --- EXIT CODE: {lixeiraDrive?.ExitCode}\n");
+                }
+            }
+        }
+
+        catch (Exception ex)
+        {
+            LogService.AddLog($"FALHA AO DELETAR O ARQUIVO! ERRO: {ex.Message}\n");
+            
+            Console.WriteLine($"FALHA AO DELETAR O ARQUIVO! ERRO: {ex.Message}");
+        }
+    }
+
+
+    public static void ClearScreenshots(string screenshots)
+    {
+        try
+        {
+            var pastaScreenhots = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C rd /s /q \"{screenshots}\"",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+            
+            pastaScreenhots?.WaitForExit();
+
+            if (!Directory.Exists(screenshots))
+            {
+                LogService.AddLog("PASTA \"Screenshots\" DELETADA COM SUCESSO!");
+            }
+            else
+            {
+                LogService.AddLog("PASTA \"Screenshots\" FALHOU AO DELETAR!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"FALHA AO DELETAR A PASTA DE SCREENSHOTS. ERRO: {ex.Message}");
+        }
+    }
+}
--- TempCleaner/Program.cs
+++ TempCleaner/Program.cs
@@ -1,152 +1,49 @@
-﻿using System.Diagnostics;
-using System.Security.Principal;
-
-namespace TempCleaner;
+﻿namespace TempCleaner;
 
 public static class Program
 {
-    private static bool IsAdmin()
-    {
-        var identity = WindowsIdentity.GetCurrent();
-        var principal = new WindowsPrincipal(identity);
-        return principal.IsInRole(WindowsBuiltInRole.Administrator);
-    }
-    
     public static void Main(string[] args)
     {
-        if (!IsAdmin())
+        if (!InitializerService.IsAdmin())
         {
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = Process.GetCurrentProcess().MainModule!.FileName,
-                UseShellExecute = true,
-                Verb = "runas"
-            };
-
-            try
-            {
-                Process.Start(startInfo);
-            }
-            catch
-            {
-                Console.WriteLine("Permissão de administrador negada.");
-            }
-
+            InitializerService.ElevateToAdmin();
             return;
         }
+
+        InitializerService.CheckLogFile();
         
-        var tempPorcentagem = Path.GetTempPath();
+        LogService.StartExecution("ARQUIVOS %TEMP%");
         
-        const string temp = "Temp";
+        CleanService.ClearFiles(PathsService.TempPorcentagem);
         
-        var windows = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+        LogService.EndExecution();
         
-        var pathTemp = Path.Combine(windows, temp);
+        LogService.StartExecution("PASTAS %TEMP%");
         
-        var pastaImagens = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        CleanService.ClearFolders(PathsService.TempPorcentagem);
 
-        var pastaScreenshots = Path.Combine(pastaImagens, "Screenshots");
+        LogService.EndExecution();
         
-        LimparArquivos(tempPorcentagem);
-        LimparPastas(tempPorcentagem);
-
-        LimparArquivos(pathTemp);
-        LimparPastas(pathTemp);
+        LogService.StartExecution("ARQUIVOS TEMP");
         
-        LimparScreenshots(pastaScreenshots);
+        CleanService.ClearFiles(PathsService.PathTemp);
         
-        LimparLixeira();
-    }
+        LogService.EndExecution();
+        
+        LogService.StartExecution("PASTAS TEMP");
+        
+        CleanService.ClearFolders(PathsService.PathTemp);
+        
+        LogService.StartExecution("PASTA SCREENSHOTS");
 
-    
-    private static void LimparArquivos(string temp)
-    {
-        foreach (var file in Directory.GetFiles(temp))
-        {
-            try
-            {
-                Console.WriteLine($"ARQUIVO ALVO: {file}");
-                File.SetAttributes(file, FileAttributes.Normal);
-                File.Delete(file);
-                Console.WriteLine("ARQUIVO DELETADO!\n");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"FALHA AO DELETAR O ARQUIVO: {file}");
-                Console.WriteLine($"ERRO: {ex.Message}\n");
-            }
-        }
-    }
+        CleanService.ClearScreenshots(PathsService.PastaScreenshots);
+        
+        LogService.EndExecution();
 
-
-    private static void LimparPastas(string temp)
-    {
-        foreach (var file in Directory.GetDirectories(temp))
-        {
-            try
-            {
-                Console.WriteLine($"PASTA ALVO: {file}");
-                Directory.Delete(file, true);
-                Console.WriteLine("PASTA DELETADA!\n");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"FALHA AO DELETAR A PASTA: {file}");
-                Console.WriteLine($"ERRO: {ex.Message}\n");
-            }
-        }
-    }
-
-
-    private static void LimparLixeira()
-    {
-        try
-        {
-            foreach (var drive in DriveInfo.GetDrives())
-            {
-                var lixeiraDrive = Process.Start(new ProcessStartInfo
-                {
-                    FileName = "cmd.exe",
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    Arguments = $"/c rd /s /q \"{drive.Name}$RECYCLE.BIN\""
-                });
-
-                lixeiraDrive?.WaitForExit();
-            }
-
-            var lixeiraWindows = Process.Start(new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                WindowStyle = ProcessWindowStyle.Hidden,
-                Arguments = "/C Clear-RecycleBin -Force"
-            });
-            
-            lixeiraWindows?.WaitForExit();
-        }
-
-        catch (Exception ex)
-        {
-            Console.WriteLine($"FALHA AO DELETAR O ARQUIVO! ERRO: {ex.Message}");
-        }
-    }
-
-    
-    private static void LimparScreenshots(string screenshots)
-    {
-        try
-        {
-            var pastaScreenhots = Process.Start(new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                WindowStyle = ProcessWindowStyle.Hidden,
-                Arguments = $"/C rd /s /q \"{screenshots}\""
-            });
-            
-            pastaScreenhots?.WaitForExit();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"FALHA AO DELETAR A PASTA DE SCREENSHOTS. ERRO: {ex.Message}");
-        }
+        LogService.StartExecution("LIXEIRAS");
+        
+        CleanService.ClearRecycleBin();
+        
+        LogService.EndExecution();
     }
 }
